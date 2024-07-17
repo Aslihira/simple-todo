aa737074212a82c9b0d64dbead163f379d51a42e
Aslihira <vrundat100@gmail.com>
2024-07-18 01:56:40 +0530

    vercel deployment

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -13,10 +13,10 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         });
     }
 
-     async validate(payload: { mail: string }) {
+     async validate(payload: { email: string }) {
          const user = await this.databaseService.user.findUnique({
             where:{
-                email: payload.mail,
+                email: payload.email,
             },
          });
          return user
