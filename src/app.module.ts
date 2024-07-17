451dd29870885a9d10e0c343b74587cb1d58faf6
Aslihira <vrundat100@gmail.com>
2024-07-18 02:37:37 +0530

    vercel deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,12 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { TodoModule } from './todo/todo.module';
 import { AuthModule } from './auth/auth.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [DatabaseModule, TodoModule, AuthModule],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder } from '@nestjs/swagger';
-import { SwaggerModule } from '@nestjs/swagger'; 
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe())
+
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+
   const config = new DocumentBuilder()
-    .setTitle('todo Rest api')
-    .setDescription('A rest api to list users todo tasks,authhorization implemented using JWT token.The api is made using nestJS')
+    .setTitle('TODO REST API')
+    .setDescription('A Todo application using REST API and authorisation using JWT, made using NEST-JS')
     .setVersion('1.0')
     .addBearerAuth()
     .addTag('todos')
     .build();
+  
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+
+  SwaggerModule.setup('/swagger', app, document, {
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+    ],
+  });
+
+  // SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
